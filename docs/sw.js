5270aeef214c7a01942d1a08af4c3ae6a96641e1
Yoshihito-Demizu <demizu@hijirigaoka.ed.jp>
2026-02-20 10:57:09 +0900

    Update sw.js

--- docs/sw.js
+++ docs/sw.js
@@ -2,7 +2,7 @@
 "use strict";
 
 // ✅ 更新のたびにここだけ変える（必ず変更！）
-const VERSION = "20260219-1";
+const VERSION = "20260219-2";
 const CACHE_NAME = `vocab-ta-${VERSION}`;
 
 // キャッシュしたい最低限
@@ -100,3 +100,4 @@ self.addEventListener("fetch", (event) => {
     return fresh;
   })());
 });
+
