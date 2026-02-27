77b6f7d7f91a8e81d36d0e04ae2592e84344c6de
Yoshihito-Demizu <demizu@hijirigaoka.ed.jp>
2026-02-27 12:56:48 +0900

    Update sw.js

--- docs/sw.js
+++ docs/sw.js
@@ -2,8 +2,8 @@
 "use strict";
 
 // ✅ 更新のたびにここだけ変える
-const VERSION = "20260225-4";
-const CACHE_NAME = `vocab-ta-${VERSION4}`;
+const VERSION = "20260225-5";
+const CACHE_NAME = `vocab-ta-${VERSION5}`;
 
 const ASSETS = [
   "./",
@@ -97,6 +97,7 @@ self.addEventListener("fetch", (event) => {
     return fresh;
   })());
 });
+
 
 
 
