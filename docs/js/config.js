682c10daa759ab32e192752ca9f9d1b64685d7d5
Yoshihito-Demizu <demizu@hijirigaoka.ed.jp>
2026-03-11 17:22:01 +0900

    Update config.js

--- docs/js/config.js
+++ docs/js/config.js
@@ -5,7 +5,7 @@
 */
 
 const SUPABASE_URL = "https://cnczakndzbqvauovoybv.supabase.co";
-const SUPABASE_ANON_KEY = "YOUR_KEY";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNuY3pha25kemJxdmF1b3ZveWJ2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjkyMjQxNzgsImV4cCI6MjA4NDgwMDE3OH0.IRszAYwh3XPqWvl6fCApjEPTuOm9x647cqzPCgmgYUA";
 
 window.client = supabase.createClient(
   SUPABASE_URL,
@@ -35,3 +35,4 @@ function getPlayerId() {
 }
 
 window.getPlayerId = getPlayerId;
+
