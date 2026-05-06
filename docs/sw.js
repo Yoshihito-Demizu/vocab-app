68b00ed28e0447d929201bc324d487571e80bea7
Yoshihito-Demizu <demizu@hijirigaoka.ed.jp>
2026-05-07 07:54:15 +0900

    Update sw.js

--- docs/sw.js
+++ docs/sw.js
@@ -7,7 +7,7 @@
  * - 画像やアイコンだけキャッシュして体感を軽くする
  */
 
-const CACHE_VERSION = "vocab-ta-2026-05-07a";
+const CACHE_VERSION = "vocab-ta-2026-05-07b";
 const ASSET_CACHE = `${CACHE_VERSION}-assets`;
 
 // 画像/アイコンなど「キャッシュしてOK」な拡張子
